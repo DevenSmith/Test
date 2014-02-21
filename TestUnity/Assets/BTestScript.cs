9c840b71ffa596c660f47cb008c49205c047117e
DevenSmith <Deven.smith@students.aie.edu.au>
2014-02-21 11:15:47 -0800

    Added Test Script B

---
+++ TestUnity/Assets/BTestScript.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// By Deven Smith
+/// 2/21/2014
+/// B test script.
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class BTestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("This is Test Script B Jason you need to edit this i will edit a");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
