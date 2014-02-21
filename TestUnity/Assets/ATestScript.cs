784864122c5c685fa617339e505fe65428931add
DevenSmith <Deven.smith@students.aie.edu.au>
2014-02-21 11:12:15 -0800

    Devens First Commit
    Made Test Script A with just a print statement in it

---
+++ TestUnity/Assets/ATestScript.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// By Deven Smith
+/// 2/21/2014
+/// A test script.
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class ATestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("This Is Devens Test Script I Dont Want To See It Change");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
